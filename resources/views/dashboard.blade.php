41f7674677925f6e52a20867cc387f91f075d39a
BrayanRF <92905668+BryanRF@users.noreply.github.com>
2023-10-16 16:58:54 -0500

    CAMBIOS NUEVOS CARGA

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="es" class="h-100" data-bs-theme="auto">
+<html lang="es" class="h-100" >
   <head><script src="{{ asset('assets/js/color-modes.js') }}"></script>
 
     <meta charset="utf-8">
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.115.4">
-    <title>Cover Template · Bootstrap v5.3</title>
+    <title>MACHINE-LEARNING</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/cover/">
 
@@ -91,68 +91,26 @@
       .bd-mode-toggle {
         z-index: 1500;
       }
+          input:required:invalid {
+        border: 1px solid red;
+    }
+
     </style>
 
 
     <!-- Custom styles for this template -->
   </head>
   <body class="d-flex h-100 text-center text-bg-dark">
-    <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
-      <symbol id="check2" viewBox="0 0 16 16">
-        <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
-      </symbol>
-      <symbol id="circle-half" viewBox="0 0 16 16">
-        <path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z"/>
-      </symbol>
-      <symbol id="moon-stars-fill" viewBox="0 0 16 16">
-        <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"/>
-        <path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z"/>
-      </symbol>
-      <symbol id="sun-fill" viewBox="0 0 16 16">
-        <path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
-      </symbol>
-    </svg>
 
-    <div class="dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle">
-      <button class="btn btn-bd-primary py-2 dropdown-toggle d-flex align-items-center"
-              id="bd-theme"
-              type="button"
-              aria-expanded="false"
-              data-bs-toggle="dropdown"
-              aria-label="Toggle theme (auto)">
-        <svg class="bi my-1 theme-icon-active" width="1em" height="1em"><use href="#circle-half"></use></svg>
-        <span class="visually-hidden" id="bd-theme-text">Toggle theme</span>
-      </button>
-      <ul class="dropdown-menu dropdown-menu-end shadow" aria-labelledby="bd-theme-text">
-        <li>
-          <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="light" aria-pressed="false">
-            <svg class="bi me-2 opacity-50 theme-icon" width="1em" height="1em"><use href="#sun-fill"></use></svg>
-            Light
-            <svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
-          </button>
-        </li>
-        <li>
-          <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark" aria-pressed="false">
-            <svg class="bi me-2 opacity-50 theme-icon" width="1em" height="1em"><use href="#moon-stars-fill"></use></svg>
-            Dark
-            <svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
-          </button>
-        </li>
-        <li>
-          <button type="button" class="dropdown-item d-flex align-items-center active" data-bs-theme-value="auto" aria-pressed="true">
-            <svg class="bi me-2 opacity-50 theme-icon" width="1em" height="1em"><use href="#circle-half"></use></svg>
-            Auto
-            <svg class="bi ms-auto d-none" width="1em" height="1em"><use href="#check2"></use></svg>
-          </button>
-        </li>
-      </ul>
-    </div>
+
+
+
 
 
 <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
   <header class="mb-auto">
-    <div>
-      <h1 class="float-md-start mb-0">PREDICCION DE ESPECIES (PRUEBA)</h1>
+    <div class="mx-auto">
+      <h1 class="float-md-start mb-0">PREDICCION DE CANCER AL CUELLO UTERINO👩‍⚕️ </h1>
       <nav class="nav nav-masthead justify-content-center float-md-end">
 
       </nav>
@@ -161,32 +119,74 @@
 
   <main class="px-3">
     <h2>Predicciones rapidas.</h2>
-    <p class="lead">Bienvenido a nuestra plataforma de predicción de nombres de hongos basada en aprendizaje automático. ¡Descubre el nombre de ese hongo misterioso!</p>
+    <p class="lead">Bienvenido a nuestra plataforma de predicción de cancer del cuello uterino basada en aprendizaje automático .</p>
 
-     <form id="imageForm" enctype="multipart/form-data" class=" g-3 align-items-center  lead">
+<form id="imageForm" enctype="multipart/form-data">
         <div class="col-md-7 col-12 mx-auto mb-3">
-            <input type="file" class="form-control" id="inlineFormInputGroupUsername" name="image" accept="image/*">
+            <input type="file" class="form-control" id="inlineFormInputGroupUsername" name="image" accept="image/*" required>
+            <div class="invalid-feedback">
+            Por favor, carga una imagen.
+        </div>
         </div>
         <div class="col-12">
+
             <button type="submit" class="btn btn-primary" id="predictButton">Predecir</button>
             <button type="button" onclick="entrenar()" class="btn btn-primary">Entrenar</button>
         </div>
+    <div class="dropdown position-fixed bottom-0 end-0 mb-5 me-3 bd-mode-toggle">
+       <select class="form-select text-sm" id="algoritmosSelect" name="algoritmo" required>
+        <!-- Opción por defecto (puedes cambiar el texto a tu preferencia) -->
+        <option value="" >Selecciona un algoritmo</option>
+    </select>
+    </div>
+</form>
 
-    </form>
   </main>
 
-  <footer class="mt-auto text-white-50">
-    <p>Cover template for <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
+
+  <footer class="mt-auto align-left
+  text-white-50">
+    <p>By @USS</a>.</p>
   </footer>
 </div>
+
 <script src="{{ asset('assets/dist/js/bootstrap.bundle.min.js') }}" rel="stylesheet"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+
  <script>
-        $(document).ready(function () {
+  $(document).ready(function() {
+        // Realizar una solicitud GET al API para obtener la lista de algoritmos
+        $.ajax({
+            url: "{{ config('app.back_url') }}/api/tipo_imagen",
+            type: 'GET',
+            success: function (data) {
+                // Iterar sobre los algoritmos y agregarlos al select
+                data.algoritmos.forEach(function(algoritmo) {
+                    $('#algoritmosSelect').append($('<option>', {
+                        value: algoritmo.id,
+                        text: "("+algoritmo.abrebiatura+") "+algoritmo.nombre
+                    }));
+                });
+            },
+            error: function (error) {
+                console.log('Error al obtener la lista de algoritmos:', error);
+            }
+        });
+    });
+   $('input[type="file"]').on('change', function() {
+            if (this.files && this.files[0]) {
+                $(this).addClass('is-valid');
+            } else {
+                $(this).removeClass('is-valid');
+            }
+        });
             $('#imageForm').on('submit', function (e) {
                 var predictButton = $('#predictButton');
                 predictButton.prop('disabled', true);
@@ -194,9 +194,17 @@
                 predictButton.html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Prediciendo...');
                 e.preventDefault();
                 var formData = new FormData(this);
+                 var fileInput = $('#inlineFormInputGroupUsername')[0];
+        if (fileInput.files.length === 0) {
+            $('.invalid-feedback').show(); // Muestra el mensaje de error
+            predictButton.html('Predecir');
+            predictButton.prop('disabled', false);
+            return;
+        }
 
+        $('.invalid-feedback').hide();
                 $.ajax({
-                    url: "{{ config('app.back_url') }}/api/especies",
+                    url: "{{ config('app.back_url') }}/api/tipo_imagen",
                     type: 'POST',
                     data: formData,
                     contentType: false,
@@ -208,7 +216,7 @@
                       if (speciesData.estado) {
                         // Display a SweetAlert with species information
                         Swal.fire({
-                            title:  `<h1 style='font-size: 24px;'> ${speciesData.name}</h1> `,
+                            title:  `<h1 style='font-size: 24px;'> ${speciesData.nombre}</h1> `,
                             showCloseButton: true,
                             showConfirmButton: false,
                             html: `
@@ -219,34 +227,7 @@
                                 </div>
                                 <div class="col-md-8">
                                 <div class="info-item">
-                                    <strong>Nombre científico:</strong> ${speciesData.scientific_name}
-                                </div>
-                                <div class="info-item">
-                                    <strong>Clase:</strong> ${speciesData.class_name}
-                                </div>
-                                <div class="info-item">
-                                    <strong>Familia:</strong> ${speciesData.family}
-                                </div>
-                                <div class="info-item">
-                                    <strong>Género:</strong> ${speciesData.genus}
-                                </div>
-                                <div class="info-item">
-                                    <strong>Orden:</strong> ${speciesData.order}
-                                </div>
-                                <div class="info-item">
-                                    <strong>Filo:</strong> ${speciesData.phylum}
-                                </div>
-                                <div class="info-item">
-                                    <strong>Información general:</strong> ${speciesData.general_information}
-                                </div>
-                                <div class="info-item">
-                                    <strong>Características físicas:</strong> ${speciesData.morphological_characteristics}
-                                </div>
-                                <div class="info-item">
-                                    <strong>Información de especie:</strong> ${speciesData.species_information}
-                                </div>
-                                <div class="info-item">
-                                    <strong>Propiedades medicinales:</strong> ${speciesData.medicinal_properties.join(", ")}
+                                    <strong>Diagnostico:</strong> ${speciesData.prediccion}
                                 </div>
                                 </div>
                             </div>
@@ -281,46 +262,60 @@
                     }
                 });
             });
-        });
-        function entrenar() {
-  // Display a spinner while the request is in progress
+function entrenar() {
   Swal.fire({
+    title: 'Entrenar',
+    html: '<input type="number" id="numEpocas" class="swal2-input" placeholder="Número de épocas">',
+    showCancelButton: true,
+    confirmButtonText: 'Entrenar',
+    preConfirm: () => {
+      return document.getElementById('numEpocas').value;
+    },
+  }).then((result) => {
+carga()
+    if (result.isConfirmed) {
+      let numEpocas = result.value;
+      if (numEpocas) {
+        $('#entrenarButton').prop('disabled', true); // Deshabilita el botón de entrenar
+        $.ajax({
+          url: "{{ config('app.back_url') }}/api/tipo_imagen",
+          type: 'PUT',
+          data: JSON.stringify({ epochs: numEpocas }),
+          contentType: 'application/json',
+          success: function (response) {
+            speciesData = response;
+            Swal.fire({
+              title: `<span style='font-size: 24px;'> Proceso exitoso</span> `,
+              text: response.mensaje,
+              icon: 'success',
+            });
+          },
+          error: function () {
+            $('#responseContainer').html('<p>Error al enviar el archivo.</p>');
+          },
+          complete: function () {
+            // Al completar la solicitud, habilita el botón de entrenar y oculta el spinner
+            $('#entrenarButton').prop('disabled', false);
+          }
+        });
+      }
+    }
+  });
+}
+
+
+function carga(){
+   Swal.fire({
     title: 'Entrenando...',
     html: '<div class="spinner-border" role="status"><span class="sr-only"></span></div>',
     allowOutsideClick: false,
     showCancelButton: false,
     showConfirmButton: false,
-    onBeforeOpen: () => {
-      Swal.showLoading();
-    },
-  });
-
-  $.ajax({
-    url: "{{ config('app.back_url') }}/api/especies",
-    type: 'PUT',
-    contentType: false,
-    processData: false,
-    success: function (response) {
-      speciesData = response;
-
-      // Close the loading spinner modal
-      Swal.close();
-
-      // Display a SweetAlert with species information
-      Swal.fire({
-        title: `<span style='font-size: 24px;'> Proceso exitoso</span> `,
-        text: response.mensaje,
-        icon: 'success',
-      });
-    },
-    error: function () {
-      // Close the loading spinner modal
-      Swal.close();
-
-      $('#responseContainer').html('<p>Error al enviar el archivo.</p>');
-    },
-  });
+   });
 }
+
+
+
 
     </script>
     <script>
